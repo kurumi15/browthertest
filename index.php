71e9ac4e433b23c3cf6699c5561fbcdca81d28de
Keisuke Kurumizawa <wal35nut@gmail.com>
2019-03-07 10:41:38 +0900

    update line login

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
    </form>
    <?php
     if (@$_GET['send']) {
-      $url="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1625867408&redirect_uri=https://browthertest.herokuapp.com/&state=keikeikei0321&scope=openid%20profile&prompt=consent&bot_prompt=normal";
+      $url="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1625867408&redirect_uri=https://browthertest.herokuapp.com/&state=keikeikei0321&scope=profile&prompt=consent&bot_prompt=normal";
       header("Location: $url");
       //echo "サブミットボタンを押しました。";
     }
